41f1d49ef68c5b34f119a817f359a68c6c7eb7f0
IX-0 <igorbaltarejo05@gmail.com>
2023-12-18 20:27:28 +0000

    .

--- javascript/log.js
+++ javascript/log.js
@@ -1,36 +1,96 @@
 function registerUser() {
-    userName = $('#').value()
-    userPassword = $('#').value()
-    users = getUsers()
-
-    userInfo = users.filter(function(element){return !(element.Name == 'username')})
-    if (userInfo.stringify() == '[]') {return false}
+    username = $('#usernameSignUp').val()
+    password = $('#passwordSignUp').val()
+    password2 = $('#password2SignUp').val()
+    phonenumber = $('#phoneNumberSignUp').val()
+    email = $('#emailSignUp').val()
+    adress = $('#adressSignUp').val()
     
-    if (userInfo.Name == undefined) {
+    var users = getUsers()
+    
+    if (username == '' || password == '' || password2 == '' || phonenumber == '' || email == '' || adress == '') {
+        alert('Missing parameter')
+        return false 
+    }
+    
+    if (password != password2) {
+        alert("Passwords don't match")
         return false
     }
-    if (userInfo.Password != userPassword){
+
+    var newUser = {
+        'Username' : username,
+        'Password' : password,
+        'PhoneNumber' : phonenumber,
+        'Email' : email,
+        'Adress' : adress,
+        'Cars': [],
+        'PendingOrders': []
+    }
+
+    var user = getUserByUsername(username)
+    if (user != undefined) {
+        alert('Já existe este username registrado')
         return false
     }
-    users.push()
-    setUsers(users)
+    else {
+        users.push(newUser)
+        setUsers(users)
+        return true
+    }
 }
 
 function removeUser(u) {
-    users = getUsers()
-    newUsers = removeItem(users,u)
+    us = getUsers()
+    newUsers = removeItem(us,u)
     setUsers(newUsers)
 }
 
-function validateLogIn() {
-    username = $('#').value()
-    userPW = $('#').value()
+function validateSignIn() {
+    var username = $('#usernameSignIn').val()
+    var password = $('#passwordSignIn').val()
 
-    
+    var u = getUserByUsername(username)
+
+    if (u == undefined) {
+        alert('Usuário não registrado')
+        return false
+    }
+
+    if (u.Password != password) {
+        alert('Password incorreta')
+        return false
+    }
+
+    localStorage.setItem('activeAccount', JSON.stringify(u))
+    return true
+}
+
+function singOut() {
+    localStorage.setItem('activeAccount', null)
+}
+
+// Getters and setters
+function getActiveAccount(){
+    return JSON.parse(localStorage.getItem('activeAccount'))
 }
 
 function getUsers() { 
-    return JSON.parse(localStorage.getItem('users'))
+    var users = localStorage.getItem('users')
+    
+    if (users == null) {
+        setUsers([])
+        return []
+    }
+
+    else {
+        return JSON.parse(users)
+    }
+}
+
+function getUserByUsername(u) { 
+    // Returns undefined is no users with said username are found
+    return getUsers().filter(value => {return value.Username == u})[0]
 }
 
 function setUsers(newUsersList) {
@@ -38,6 +98,10 @@ function setUsers(newUsersList) {
 }
 
 // Helper functions
+function isInArray(item,array) {
+    return JSON.stringify(array).includes(JSON.stringify(item))
+}
+
 function compareArrays(a,b) {
     return a.length === b.length && JSON.stringify(a) == JSON.stringify(b)
 }
@@ -47,3 +111,24 @@ function removeItem(arr, item) {
         return !compareArrays(item,element) 
     })
 }
+
+$(document).ready(function(){
+
+    $('#signInForm').on('submit', event => {
+        if (!validateSignIn()) {
+        event.preventDefault()
+        event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+    })
+
+    $('#signUpForm').on('submit', event => {
+        if (!registerUser()) {
+        event.preventDefault()
+        event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+    })
+})
